ef5f96aeceb6c744770dd869674380cee92a702b
Mahdi bin Iqbal <151855877+mahadihasan9@users.noreply.github.com>
2026-09-11 00:29:31 +0600

    Rename JNI function to getMessage

--- src/jni/native.cpp
+++ src/jni/native.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_myfirstapp_MainActivity_getSms(JNIEnv* env, jobject /* this */) {
+Java_com_example_myfirstapp_MainActivity_getMessage(JNIEnv* env, jobject /* this */) {
     std::string message = "Hello Brother, Welcome ";
     return env->NewStringUTF(message.c_str());
 }
