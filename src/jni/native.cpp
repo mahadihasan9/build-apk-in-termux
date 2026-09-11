3bad3ad458ebd1c67e104d7c54d79ddb10c81a3c
Mahdi bin Iqbal <islammdmahadi943@gmail.com>
2026-09-11 19:27:28 +0600

    Update greeting message in JNI function

--- src/jni/native.cpp
+++ src/jni/native.cpp
@@ -3,6 +3,6 @@
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_myfirstapp_MainActivity_getMessage(JNIEnv* env, jobject /* this */) {
-    std::string message = "Hello Brother, Welcome ";
+    std::string message = "Hello Brother, Welcome to my apk";
     return env->NewStringUTF(message.c_str());
 }
