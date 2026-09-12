b4ae9204dcb91cb18b264f00b455a5937ac86025
Mahdi bin Iqbal <islammdmahadi943@gmail.com>
2026-09-12 11:14:44 +0600

    Format time message in JNI function to include a label

--- src/jni/native.cpp
+++ src/jni/native.cpp
@@ -6,6 +6,6 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_myfirstapp_MainActivity_getMessage(JNIEnv* env, jobject /* this */) {
     std::time_t result = std::time(nullptr);
     char *time = std::ctime(&result);
-    std::string message = std::string(time);
+    std::string message = "Current time: " + std::string(time);
     return env->NewStringUTF(message.c_str());
 }
